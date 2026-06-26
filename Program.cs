3dd0967ce8bafc029c21e534b5118b010202a036
Tuncay Babayev <tbabayev37t@gmail.com>
2026-06-26 18:29:34 +0400

    setup CORS and build functional frontend index page

--- Program.cs
+++ Program.cs
@@ -3,6 +3,15 @@ using LibraryManagment.Profiles;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<LibraryManagementContext>(options =>
@@ -27,6 +36,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+app.UseCors("AllowAll");
 app.MapControllers();
 
 app.Run();
